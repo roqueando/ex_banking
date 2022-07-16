e744e28d34a5c415c6c37eaadcb5cb9e4ff636fc
Vitor Roque <vitor.roquep@gmail.com>
2022-07-16 01:13:40 -0300

    feat: add operations state

--- lib/operation.ex
+++ lib/operation.ex
@@ -10,12 +10,20 @@ defmodule Operation do
   end
 
   def handle_cast({:insert, {user, type}}, state) do
-    # TODO: send a process to remove a operation after one second
-    # Process.send_after()
+    Process.send_after(self(), {:free_operation, user}, 1000)
     {:noreply, [%{user: user, type: type, status: :pending} | state]}
+  end
+
+  def handle_info({:free_operation, user}, state) do
+    operations = find_operations_by_user(user, state)
+    state = List.delete_at(operations, length(operations) - 1)
+    {:noreply, state}
   end
 
   def insert(user, type) do
     GenServer.cast(__MODULE__, {:insert, user, type})
   end
+
+  defp find_operations_by_user(user, operations),
+    do: Enum.filter(operations, fn %{user: find_user} -> user == find_user end)
 end
